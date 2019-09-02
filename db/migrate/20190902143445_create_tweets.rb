fc6e3dc0c80ea1acf699b3d8ba45925609e55769
titoignacio <h.villalobosm03@gmail.com>
2019-09-02 11:15:38 -0400

    segundo commit

---
+++ db/migrate/20190902143445_create_tweets.rb
@@ -0,0 +1,10 @@
+class CreateTweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+      t.string :content
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
